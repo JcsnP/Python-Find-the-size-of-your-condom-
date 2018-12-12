53988168ad4611bad45e2562ffb108e262a93d06
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2018-12-12 22:13:16 +0700

    Add files via upload

---
+++ condom.py
@@ -0,0 +1,30 @@
+print("---------------------Make By JcsnP--------------------")
+print("---------------What is your condom size---------------")
+print("Enter your Circumference (mm) :")
+sum = int(input(""))
+#Sum Output
+#IF ELIF ELSE
+if sum == 0:
+	print("Error!! Enter more than 1")
+elif sum <= 110:
+	print("Oops!!! You have a small cock 5555555.")
+elif sum <= 120:
+	print("49 mm.")
+elif sum <= 130:
+	print("52 mm.")
+elif sum <= 140:
+	print("54 mm.")
+elif sum <= 150:
+	print("56 mm.")
+elif sum >= 151:
+	print("Oh !!! You have a big cock.")
+else:
+	print("End")
+print("--------------------------End-------------------------")
+'''
+		CopyRight 2018. 				ᕙ(⇀‸↼‶)ᕗ
+		Chitsanupong Paenyoi 			♥‿♥
+		JcsnP 							ಠ~ಠ
+		12/12/2018 Time: 21:48 UTC+07:00 Thailand.
+		Nakhon Si Thammarat,Lansaka,Thadee
+'''
